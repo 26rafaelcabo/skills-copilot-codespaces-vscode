8c612b7991dd19a03fc1ab3704e1127f439390cd
26rafaelcabo <150359076+26rafaelcabo@users.noreply.github.com>
2024-02-05 13:36:26 +0000

    Copilot first commit

--- .devcontainer/skills.js
+++ .devcontainer/skills.js
@@ -1,3 +1,6 @@
+// FILEPATH: /workspaces/skills-copilot-codespaces-vscode/.devcontainer/skills.js
+// GitHub Copilot generated code
+
 function calculateNumbers(var1, var2) {
     return var1 + var2;
-}   //teste
+}
