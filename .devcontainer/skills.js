0105d6aaa2e7dd9127be49ea8caf42ef73857c06
26rafaelcabo <150359076+26rafaelcabo@users.noreply.github.com>
2024-02-05 13:54:42 +0000

    deletado arquivo

--- .devcontainer/skills.js
+++
@@ -1,5 +0,0 @@
-// FILEPATH: /workspaces/skills-copilot-codespaces-vscode/.devcontainer/skills.js
-// GitHub Copilot generated code
-function calculateNumbers(var1, var2) {
-    return var1 + var2;
-}
